e4c12bf5f56c7d26b8220dee82ce17298acf3d50
helpau <helpau@test.com>
2024-12-15 15:10:51 +0500

    0.19.1

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function clearHistory() {
 // init Pyodide and show sys.version when it's loaded successfully
 async function main() {
   let pyodide = await loadPyodide({
-    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.20.0/full",
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.1/full/",
   });
   output.value = pyodide.runPython(`
       import sys
